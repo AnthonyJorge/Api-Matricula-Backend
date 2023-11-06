80e6d776105efa96e90c950a471b34a568accad9
Jorge Anthony <128651289+AnthonyJorge@users.noreply.github.com>
2023-11-06 17:08:46 -0500

    first commit

---
+++ src/main/java/com/moviles/ProyectoBackendMovilesApplication.java
@@ -0,0 +1,13 @@
+package com.moviles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBackendMovilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBackendMovilesApplication.class, args);
+	}
+
+}
